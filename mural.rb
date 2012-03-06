6ee8a188141a5b5e6f78d8007bf3a301c010cb46
Andre Fonseca <aoqfonseca@gmail.com>
2012-03-05 22:16:41 -0300

    acertos de deps no codigo

--- mural.rb
+++ mural.rb
@@ -1,12 +1,10 @@
 require 'rubygems'
 gem 'haml'
-gem 'rcov'
 gem 'arrayextension'
 gem 'sinatra'
 
 require 'yaml'
 require 'haml'
-require 'rcov'
 require 'arrayextension'
 require 'sinatra'
 require File.join(File.dirname(__FILE__), 'models', 'phrases')
